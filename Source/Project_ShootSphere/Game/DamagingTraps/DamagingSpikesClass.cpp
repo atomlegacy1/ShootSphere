7f4f0a5adec18170690f9ac38e0916e1cdf6e99f
atomlegacy1 <core85683@gmail.com>
2023-04-05 07:46:59 -0400

    Commit 05.04.23
    1)Update SpikesSphere model and add spikes rotation
    2)Update SpikesTrap model, prepare to spikes out

--- Source/Project_ShootSphere/Game/DamagingTraps/DamagingSpikesClass.cpp
+++ Source/Project_ShootSphere/Game/DamagingTraps/DamagingSpikesClass.cpp
@@ -5,15 +5,39 @@
 #include "Project_ShootSphere/Game/PlayerCharacter/ShootSpherePlayerCharacter_Base.h"
 #include "Kismet/GameplayStatics.h"
 
+ADamagingSpikesClass::ADamagingSpikesClass()
+{
+	MovingSpikesMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("MovingSpikesMesh"));
+	MovingSpikesMesh->SetupAttachment(RootComp);
+	SpikesOutCollision = CreateDefaultSubobject<UBoxComponent>(FName("SpikesOutCollision"));
+	SpikesOutCollision->SetupAttachment(RootComp);
+}
 
 void ADamagingSpikesClass::BeginPlay()
 {
 	Super::BeginPlay();
+
+	SpikesMovingRate = 2;
+	RangeToOut = 95;
+	FVector TempVector{0,RangeToOut,0};
+	SpikesMovingVector={0,SpikesMovingRate,0};
+	
+	SpikesStartLocation = MovingSpikesMesh->GetComponentLocation();
+	SpikesOutLocation = MovingSpikesMesh->GetComponentLocation() + TempVector;
 	
 	CollisionComp->OnComponentBeginOverlap.AddDynamic(this,&ThisClass::StartApplyDamage);
 	CollisionComp->OnComponentEndOverlap.AddDynamic(this,&ThisClass::StopApplyDamage);
+
+	SpikesOutCollision->OnComponentBeginOverlap.AddDynamic(this,&ThisClass::MoveSpikesUp);
+	SpikesOutCollision->OnComponentEndOverlap.AddDynamic(this,&ThisClass::MoveSpikesDown);
 }
 
+void ADamagingSpikesClass::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	SpikesCurrentLocation = MovingSpikesMesh->GetComponentLocation();
+}
 
 void ADamagingSpikesClass::StartApplyDamage(class UPrimitiveComponent* OverlappedComp,class AActor* OtherActor, class UPrimitiveComponent* OtherComp,
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
@@ -24,7 +48,6 @@ void ADamagingSpikesClass::StartApplyDamage(class UPrimitiveComponent* Overlappe
 		GetWorldTimerManager().SetTimer(THDamageApply,this,&ThisClass::ApplyDamageToActor,2.5f,true);
 	}
 }
-
 void ADamagingSpikesClass::StopApplyDamage(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
@@ -33,9 +56,52 @@ void ADamagingSpikesClass::StopApplyDamage(UPrimitiveComponent* OverlappedComp, 
 		GetWorldTimerManager().ClearTimer(THDamageApply);
 	}
 }
-
 void ADamagingSpikesClass::ApplyDamageToActor()
 {
 	UGameplayStatics::ApplyDamage(CharacterToDamage,DamageToCause,GetInstigatorController(),
 this,UDamageType::StaticClass());
+}
+
+void ADamagingSpikesClass::MoveSpikesUp(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor == UGameplayStatics::GetPlayerCharacter(GetWorld(),0))
+	{
+		isCharacterOverlap = true;
+		if (SpikesCurrentLocation!=SpikesStartLocation)
+		{
+			MoveSpikes();
+			//GetWorldTimerManager().SetTimer(THSpikesMove,this,&ThisClass::MoveSpikes,1.0f,true);
+		}
+		else
+		{
+			GetWorldTimerManager().ClearTimer(THSpikesMove);
+		}
+	}
+}
+void ADamagingSpikesClass::MoveSpikesDown(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor == UGameplayStatics::GetPlayerCharacter(GetWorld(),0))
+	{
+		isCharacterOverlap = false;
+		if (SpikesCurrentLocation!=SpikesOutLocation)
+		{
+			MoveSpikes();
+			//GetWorldTimerManager().SetTimer(THSpikesMove,this,&ThisClass::MoveSpikes,1.0f,true);
+		}
+		else
+		{
+			GetWorldTimerManager().ClearTimer(THSpikesMove);
+		}
+	}
+}
+void ADamagingSpikesClass::MoveSpikes()
+{
+	if (isCharacterOverlap)
+	{
+		MovingSpikesMesh->SetWorldLocation(SpikesCurrentLocation+SpikesMovingVector);
+	}
+	else
+	{
+		MovingSpikesMesh->SetWorldLocation(SpikesCurrentLocation-SpikesMovingVector);
+	}
 }
--- Source/Project_ShootSphere/Game/DamagingTraps/DamagingSpikesClass.h
+++ Source/Project_ShootSphere/Game/DamagingTraps/DamagingSpikesClass.h
@@ -17,18 +17,42 @@ class PROJECT_SHOOTSPHERE_API ADamagingSpikesClass : public ADamagingTrapsBase
 protected:
 	UPROPERTY(EditDefaultsOnly,Category = "SpikesSettings", meta=(ClampMin = 0))
 	float DamageToCause{10};
+	UPROPERTY(EditDefaultsOnly,Category = "SpikesSettings", meta=(ClampMin = 0))
+	float SpikesMovingRate;
+	
 private:
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMeshComponent* MovingSpikesMesh;
+	UPROPERTY(EditDefaultsOnly)
+	UBoxComponent* SpikesOutCollision;
+	
 	UPROPERTY()
 	class AActor* CharacterToDamage;
 	UPROPERTY()
 	FTimerHandle THDamageApply;
+	
+	UPROPERTY()
+	FTimerHandle THSpikesMove;
+	UPROPERTY()
+	FVector SpikesOutLocation;
+	UPROPERTY()
+	FVector SpikesStartLocation;
+	UPROPERTY()
+	FVector SpikesCurrentLocation;
+	UPROPERTY()
+	FVector SpikesMovingVector;
+	UPROPERTY()
+	float RangeToOut;
+	UPROPERTY()
+	bool isCharacterOverlap{false};
 
 #pragma endregion
 
 #pragma region Actions
 
 public:
-	virtual void BeginPlay() override;
+	ADamagingSpikesClass();
+	virtual void Tick(float DeltaSeconds) override;
 	
 	UFUNCTION()
 	void StartApplyDamage(class UPrimitiveComponent* OverlappedComp,class AActor* OtherActor, class UPrimitiveComponent* OtherComp,
@@ -36,8 +60,24 @@ public:
 	UFUNCTION()
 	void StopApplyDamage(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 		int32 OtherBodyIndex);
+
+	
+	UFUNCTION()
+	void MoveSpikesUp(class UPrimitiveComponent* OverlappedComp,class AActor* OtherActor, class UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void MoveSpikesDown(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
 	UFUNCTION()
 	void ApplyDamageToActor();
+
+	UFUNCTION()
+	void MoveSpikes();
 
 #pragma endregion 
 	
